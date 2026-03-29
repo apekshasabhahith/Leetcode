e597559ab48d00ff1cf21d0a86f2e7e7ee89d672
Apeksha Sabhahith <apekshasabhahith@gmail.com>
2026-03-29 23:03:53 +0530

    missing number game

---
+++ java/missingnumber(BS).java
@@ -0,0 +1,17 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int start=0;
+        int end=n;
+        while(start>=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==mid){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+    return start;
+    }
+}
